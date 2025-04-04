92eebee3db412a08d9c8e41544def1c4e85f8230
Rhys Harris <rhys.harris@hotmail.co.uk>
2025-04-04 11:02:32 +0100

    test as server is broke

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ class Config:
      # Set these in dockerfile or env as otherwise it uses unsecure defaults
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'development-key-change-in-production'
     # OLD MONGO_URI = os.environ.get('MONGODB_URI') or 'mongodb://localhost:27017/security_hub'
-    MONGO_URI = os.environ.get('MONGODB_URI') or 'mongodb://admin:password@mongodb:27017/security_hub?authSource=admin' # new
+    MONGO_URI = os.environ.get('MONGODB_URI') or 'mongodb://rhys:password@mongodb:27017/security_hub?authSource=admin' # new
