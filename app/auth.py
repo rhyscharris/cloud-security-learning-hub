eaed0aa5fb9cb375c2def867b187c216c9481e5b
Rhys Harris <rhys.harris@hotmail.co.uk>
2025-04-10 20:30:36 +0100

    red test

--- app/auth.py
+++ app/auth.py
@@ -7,10 +7,10 @@ from app import mongo # From __init__.py, import mongo
 auth = Blueprint('auth', __name__) # Creates a Blueprint (component/module) called 'auth'
 
 # WARNING: Demo A - this is intentionally for testing
-#password = "secret123" # this is only here to demo Ruff blocking this.
+password = "secret123" # this is only here to demo Ruff blocking this.
 
 # WARNING: Demo B - this is intentionally for testing
-#eval("print('This is insecure')")
+eval("print('This is insecure')")
 
 @auth.route('/login', methods=['GET', 'POST']) # When user goes to domain or localhost/login, and does POST/GET, do this
 def login():
